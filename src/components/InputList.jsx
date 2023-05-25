6cf6938f71e2b424b92a815c2304880ce6d4d1f1
Aratchaporn Jiamwijitkul <praearatchaporn@gmail.com>
2023-05-25 18:05:23 +0700

    Add push new url parameter functionality and copy url button

--- src/components/InputList.jsx
+++ src/components/InputList.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 
-const InputList = ({ label, placeholder, value }) => {
+const InputList = ({ name, label, placeholder, value, handleChange }) => {
   return (
-    <div className="grid grid-cols-7 items-center space-y-3">
-      <p className="text-right mr-3 text-white font-medium col-span-2">
+    <div className="grid grid-cols-7 items-center">
+      <p className="text-right my-3 mr-3 text-white font-medium col-span-2">
         {label} :
       </p>
       <input
         className="px-5 py-2 rounded-md col-span-5"
         type="text"
+        name={name}
         placeholder={placeholder}
-        defaultValue={value}
+        value={value}
+        onChange={handleChange}
       />
     </div>
   );
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -1,19 +1,58 @@
 import React, { useEffect, useState } from "react";
 import InputList from "../components/InputList";
+import { BsLink45Deg } from "react-icons/bs";
+import { BsCheckLg } from "react-icons/bs";
 
 const Registration = () => {
-  const [inputData, setInputData] = useState({});
+  const [inputData, setInputData] = useState({
+    firstName: "",
+    lastName: "",
+    phoneNo: "",
+    email: "",
+    ref: "",
+  });
+  const [urlCopied, setUrlCopied] = useState(false);
 
+  ////////// GET URL PARAMETERS //////////
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     setInputData({
-      firstName: urlParams.get("firstName"),
-      lastName: urlParams.get("lastName"),
-      phoneNo: urlParams.get("phoneNo"),
-      email: urlParams.get("email"),
-      ref: urlParams.get("ref"),
+      firstName: urlParams.get("firstName") ? urlParams.get("firstName") : "",
+      lastName: urlParams.get("lastName") ? urlParams.get("lastName") : "",
+      phoneNo: urlParams.get("phoneNo") ? urlParams.get("phoneNo") : "",
+      email: urlParams.get("email") ? urlParams.get("email") : "",
+      ref: urlParams.get("ref") ? urlParams.get("ref") : "",
     });
   }, []);
+
+  ////////// HANDLE CHANGE INPUT //////////
+  const handleChangeInput = (event) => {
+    //SET DATA
+    setInputData((prev) => {
+      return { ...prev, [event.target.name]: event.target.value };
+    });
+
+    //GET CURRENT URL PARAMS AND SET THE CHANGED ONE
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set(event.target.name, event.target.value);
+
+    //CREATE NEW URL AND PUSH
+    const newUrl = `${window.location.origin}${
+      window.location.pathname
+    }?${urlParams.toString()}`;
+    window.history.pushState({}, "", newUrl);
+    // console.log("window.location.origin =", window.location.origin);
+    // console.log("window.location.pathname =", window.location.pathname);
+  };
+
+  ////////// HANDLE COPY URL //////////
+  const handleCopyURL = () => {
+    navigator.clipboard.writeText(window.location.href);
+    setUrlCopied(true);
+    setTimeout(() => {
+      setUrlCopied(false);
+    }, 2000);
+  };
 
   console.log("inputData =", inputData);
 
@@ -27,30 +66,57 @@ const Registration = () => {
       {/* FROM */}
       <div className="m-4">
         <InputList
+          name="firstName"
           label="First Name"
           placeholder="Input first name"
           value={inputData.firstName}
+          handleChange={handleChangeInput}
         />
         <InputList
+          name="lastName"
           label="Last Name"
           placeholder="Input last name"
           value={inputData.lastName}
+          handleChange={handleChangeInput}
         />
         <InputList
+          name="phoneNo"
           label="Phone No"
           placeholder="Input phone number"
           value={inputData.phoneNo}
+          handleChange={handleChangeInput}
         />
         <InputList
+          name="email"
           label="Email"
           placeholder="Input email address"
           value={inputData.email}
+          handleChange={handleChangeInput}
         />
         <InputList
+          name="ref"
           label="Ref"
           placeholder="Input reference"
           value={inputData.ref}
+          handleChange={handleChangeInput}
         />
+      </div>
+
+      {/* COPY URL BUTTON */}
+      <div className="flex flex-col items-center mt-6">
+        <button
+          className="flex justify-center items-center w-[200px] bg-[#727b81] px-6 py-2 rounded-full border-2 border-white text-white font-semibold shadow-md hover:shadow-lg hover:bg-[#929a9f] focus:bg-[#a8b0b5]"
+          onClick={handleCopyURL}
+        >
+          <BsLink45Deg className="mr-1 text-2xl" />
+          Copy URL
+        </button>
+        {urlCopied && (
+          <p className="mt-2 flex items-center text-white">
+            <BsCheckLg className="mr-1" />
+            URL Copied!
+          </p>
+        )}
       </div>
     </div>
   );
